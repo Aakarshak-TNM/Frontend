b0c794aa66d68741723539b623ef34ac6b17e872
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-03-05 13:14:20 -0500

    Added CodeDeploy!-5

---
+++ scripts/before_install.sh
@@ -0,0 +1,28 @@
+
+# clean codedeploy-agent files for a fresh install
+sudo rm -rf /home/ubuntu/install
+
+# install CodeDeploy agent
+sudo apt-get -y update
+sudo apt-get -y install ruby
+sudo apt-get -y install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install 
+sudo ./install auto
+
+# update os & install python3
+sudo apt-get update
+# sudo apt-get install -y python3 python3-dev python3-pip python3-venv
+# pip install --user --upgrade virtualenv
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# delete app
+sudo rm -rf /home/ubuntu/Frontend
+
